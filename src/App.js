66c32277faaed70050e6e62440459c0f4c699263
Meg Walters <megwalterss@gmail.com>
2017-04-07 11:45:13 -0500

    Added additional picture and list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './party_cat.jpeg';
+import music_cat from './music_cat.jpeg'
 import './App.css';
 
 class App extends Component {
@@ -8,11 +9,18 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>This is an awesome react web page</h2>
+          <h2>Party cat's page of parties</h2>
         </div>
         <p className="App-intro">
-          This is a code change.  Yay!
+          Compilation of party cat's suggestions to have a great weekend
         </p>
+        <p>
+          <ul> 
+          <li> Get some great tunes
+            </li>
+          </ul>
+          <img src={music_cat}/>
+          </p>
       </div>
     );
   }
